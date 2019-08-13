cddc773366ee1d6385b3d1aa8b5310c253d50d0b
Tim Stadler <timstadler905@googlemail.com>
2019-08-13 22:35:31 +0200

    Show crossair

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,9 @@ public class PlayerController : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
-
+    Cursor.visible = true;
+    var tex = Resources.Load<Texture2D>("Sprites/Crossair");
+    Cursor.SetCursor(tex, new Vector2(tex.width / 2, tex.height / 2), CursorMode.Auto);
   }
 
   // Update is called once per frame
