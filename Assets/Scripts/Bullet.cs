cb03e0571b0bfce2a191e76cb57b36ee5eb19eae
Tim Stadler <timstadler905@googlemail.com>
2019-08-16 13:58:43 +0200

    Add lifetime script

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,13 +4,11 @@ public class Bullet : MonoBehaviour
 {
   public float speed = 2f;
   public Rigidbody2D rb;
-  public float Lifetime = 200f;
 
   // Start is called before the first frame update
   void Start()
   {
     rb.velocity = transform.up * speed; // up instead of forward, because this is 2D
-    Destroy(gameObject, Lifetime);
   }
 
   private void OnTriggerEnter2D(Collider2D collision)
